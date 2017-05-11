b3ada36eccd2befac92fa3f358005107f913c66e
Filiz Altıntürk <filiz.altinturk@nesine.com>
2017-05-11 13:34:38 +0300

    vue.js added and browser sync set

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch("./sass/*.scss", ['sass']);
     gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
