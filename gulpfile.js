f5359a46fb7b0f4a3e3584fb93a3e06be07e08a1
Filiz Altıntürk <filiz.altinturk@nesine.com>
2017-05-11 13:06:27 +0300

    gulp browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,21 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create();
 
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./sass/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+gulp.task('default', ['serve']);
 
-
-gulp.task('dev', ['sass:watch']);
